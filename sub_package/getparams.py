8feee5e382699f4f5e3d6365d71f62cd4932a167
Yun-An Li <awdr4634@gmail.com>
2022-03-18 16:45:57 +0800

    Update getparams.py
    read new parameters in params.ini

--- sub_package/getparams.py
+++ sub_package/getparams.py
@@ -28,6 +28,25 @@ class ReadCube:
             rms[i] = np.sqrt(np.nanmean(np.power(self.data[i],2)))
         self.rms = rms
 
+    def select_chans(self,first=0,last=99):
+        if len(self.data.shape) == 4:
+            if np.size(self.data,0) != 1:
+                self.data = self.data[first:last+1,0,:,:]
+                self.rms = self.rms[first:last+1,0,:,:]
+            else:
+                self.data = self.data[0,first:last+1,:,:]
+                self.rms = self.rms[0,first:last+1,:,:]
+        else:
+            self.data = self.data[first:last+1,:,:]
+            self.rms = self.rms[first:last+1,:,:]
+        self.nchan = last-first+1
+        if self.hd['CTYPE3'] == 'VRAD':
+            self.velstart = self.velstart + self.velwidth*first
+            self.hd['CRVAL3'] = self.velstart
+        elif self.hd['CTYPE3'] == 'FREQ':
+            self.freqstart = self.freqstart + self.freqwidth*first
+            self.hd['CRVAL3'] = self.freqstart
+
 class Parameters:
     def __init__(self,inifile):
         para = configparser.ConfigParser()
@@ -38,7 +57,14 @@ class Parameters:
         except:
             imglist = para.get('file','imglist').replace("'",'"')
             self.imglist = json.loads(imglist)
+        try:
+            self.chanrange = json.loads(para.get('file','chanrange'))
+        except:
+            chanrange = para.get('file','chanrange').replace("'",'"')
+            self.chanrange = json.loads(chanrange)
         pixsize = para.getfloat('fixed', 'pixsize')
+        samplesize = para.getfloat('fixed', 'samplesize')
+        imgrange = para.get('fixed', 'imgrange').split(',')
         dist = para.getfloat('fixed', 'dist')
         qr = para.getfloat('fixed', 'qr')
         qd = para.getfloat('fixed', 'qd')
@@ -51,12 +77,12 @@ class Parameters:
         vlsr = para.get('free', 'vlsr').split(',')
         incl = para.get('free', 'incl').split(',')
         pa = para.get('free', 'pa').split(',')
-        vr0sin = para.get('free', 'vr0sin').split(',')
+        vr0 = para.get('free', 'vr0').split(',')
         linewidth = para.get('free', 'linewidth').split(',')
         ri = para.get('free', 'ri').split(',')
         ro = para.get('free', 'ro').split(',')
-        self.fixed = {'pixsize':pixsize,'dist':dist,'qr':qr,
-                      'qd':qd,'r0':r0,'bmaj':bmaj,'bmin':bmin,
+        self.fixed = {'pixsize':pixsize,'samplesize':samplesize,'imgrange':imgrange,
+                      'dist':dist,'qr':qr,'qd':qd,'r0':r0,'bmaj':bmaj,'bmin':bmin,
                       'bpa':bpa,'x0':x0,'y0':y0
                      }
         try:
@@ -64,7 +90,7 @@ class Parameters:
             'vlsr':{'value':float(vlsr[0]),'vary':vlsr[1],'bound':[float(vlsr[2]),float(vlsr[3])]},
             'incl':{'value':float(incl[0]),'vary':incl[1],'bound':[float(incl[2]),float(incl[3])]},
             'pa':{'value':float(pa[0]),'vary':pa[1],'bound':[float(pa[2]),float(pa[3])]},
-            'vr0sin':{'value':float(vr0sin[0]),'vary':vr0sin[1],'bound':[float(vr0sin[2]),float(vr0sin[3])]},
+            'vr0':{'value':float(vr0[0]),'vary':vr0[1],'bound':[float(vr0[2]),float(vr0[3])]},
             'linewidth':{'value':float(linewidth[0]),'vary':linewidth[1],'bound':[float(linewidth[2]),float(linewidth[3])]},
             'ri':{'value':float(ri[0]),'vary':ri[1],'bound':[float(ri[2]),float(ri[3])]},
             'ro':{'value':float(ro[0]),'vary':ro[1],'bound':[float(ro[2]),float(ro[3])]},
@@ -90,5 +116,3 @@ class Parameters:
             else:
                 sys.exit("InputError: 'variation' should be 'True' or 'False'")
         self.df = len(self.para0)
- 
-    
